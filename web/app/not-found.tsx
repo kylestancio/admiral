8e3e04d84f8c438e163c46030b7c7f896ae30116
kylestancio <kylestancio@gmail.com>
2023-10-11 11:14:50 +0900

    [feat] create custom 404 page

---
+++ web/app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div className='w-full h-[calc(100vh-3.5rem)] flex'>
+      <div className='m-auto'>Page not found</div>
+    </div>
+  )
+}
