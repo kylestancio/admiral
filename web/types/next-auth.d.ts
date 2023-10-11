6b3c8465e2bb05f45ad9494aea941576ff4f42fa
kylestancio <kylestancio@gmail.com>
2023-10-11 11:13:53 +0900

    [fix] rename next-auth types

--- web/types/next-auth.d.ts
+++ web/types/next-auth.d.ts
@@ -1,9 +1,11 @@
+import { UserRole } from "@prisma/client"
 import NextAuth, { DefaultSession } from "next-auth"
 
 declare module "next-auth" {
   interface Session {
     user: {
       username: string,
+      role: UserRole[]
     } & DefaultSession["user"]
   }
 }
