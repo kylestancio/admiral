e521653cbeb18b461f3247d3a9a2450b81e26cd6
Kyle Stephen Tancio <kylestancio@gmail.com>
2023-10-13 00:26:56 +0900

    [feat] create 'Add User' button

--- web/app/users/page.tsx
+++ web/app/users/page.tsx
@@ -4,6 +4,8 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import { verifyAccessRole } from '@/lib/utils';
 import NotAllowedAlert from '../not-allowed';
+import Link from 'next/link';
+import { Plus } from 'lucide-react';
 
 const ROLES_ALLOWED = [
   "SUPERUSER",
@@ -28,7 +30,15 @@ export default async function UsersPage() {
 
       { session && session.user && userCanAccess && 
         <div className='container mx-auto mt-10'>
-          <h1 className='text-2xl font-medium'>Employees</h1>
+          <div className='flex justify-between'>
+            <h1 className='text-2xl font-medium'>Employees</h1>
+            <Link 
+              href={`${process.env.NEXT_PUBLIC_URL}/users/create`}
+              className='px-2 py-1 bg-zinc-950 text-zinc-100 hover:bg-zinc-800 dark:bg-zinc-100 dark:text-zinc-950 dark:hover:bg-zinc-300 rounded-md flex'>
+              <Plus className='inline my-auto' />
+              <p className='inline my-auto'>Add User</p>
+            </Link>
+          </div>
           <EmployeesContainer />
         </div>
       }
